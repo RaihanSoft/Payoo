87709ef46641731e764849a6884823b478bcb2f4
Raihan <aboraihan14@gmail.com>
2024-09-18 22:45:40 +0600

    validation added

--- login.js
+++ login.js
@@ -1,25 +1,43 @@
 const btn = document.getElementById('btn')
-
-const inpPassword = document.getElementById('inpPassword')
-
-
 btn.addEventListener('click', function (event) {
-
     event.preventDefault
-    const inpNumber = document.getElementById('inpNumber')
-    console.log(inpNumber.value);
+    // phone number 
+    
+    const inpNumber = document.getElementById('inpNumber').value
+    console.log(inpNumber);
 
+    // 4 Digit pin 
+    const inpPassword = document.getElementById('inpPassword').value
+    console.log(inpPassword);
+
+    if(inpNumber === '019' && inpPassword === '1234'){
+        console.log("your log in");
+
+    }
+    else{
+        alert("Wrong phone number or pin");
+    }
 
 })
 
-inpNumber.addEventListener('keyup', function (event) {
-
-    //    const  inpNumber = event.target.value;
-    console.log(event.target.value);
 
 
 
 
-})
+
+
+
+
+
+
+// inpNumber.addEventListener('keyup', function (event) {
+
+//     //    const  inpNumber = event.target.value;
+//     console.log(event.target.value);
+
+
+
+
+// })
 
 
