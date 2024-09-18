d098a6c8ccbb64cdbe550fdc27bfeaf0fb7d32b9
Raihan <aboraihan14@gmail.com>
2024-09-19 01:05:57 +0600

    cash out added

--- cashout.js
+++ cashout.js
@@ -1,37 +1,34 @@
-document.getElementById('btn').addEventListener('click', function(event){
+document.getElementById('btn1').addEventListener('click', function(event){
     event.preventDefault()
 
 //amount
-    const inpAmount2 = document.getElementById('amount').value
+    const inpAmount2 = document.getElementById('amount1').value
     const inpAmount =  Number(inpAmount2)
         console.log('input amount',inpAmount);
 
 // pin number
-    const inpPin = document.getElementById('inpPin')
+    const inpPin = document.getElementById('inpPin1')
     const pinNumber =  inpPin.value;
 
 
      if(pinNumber === '4321'){
 
-        const currentAmount = document.getElementById('currentAmount')
+        const currentAmount = document.getElementById('currentAmount1')
         const currenA = Number(currentAmount.innerText);
         console.log('current amount',currenA);
 
 
-        let newBalance =  currenA + inpAmount
+        let newBalance =  currenA - inpAmount
         console.log("Total Amount",newBalance);
 
 
         currentAmount.innerText = newBalance
-     
-
-
         
 
 
      }
      else{
-        alert("Invalid Pin Number")
+        alert("Faild To Cash Out. Please try again leater..")
      }
 
 
